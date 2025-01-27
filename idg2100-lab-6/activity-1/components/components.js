c1f3ad18edf27d64904c6f989fe39633f3db9c6c
Nora <norajr@storroe.no>
2025-01-27 13:43:28 +0100

    lecture 6

---
+++ idg2100-lab-6/activity-1/components/components.js
@@ -0,0 +1,9 @@
+//idk what is going on, might have to look at the lecture for this one
+class CurrencyConverter extends HTMLElement {
+    constructor() {
+        super()
+        let shadowRoot = attachShadow();
+    }
+}
+
+//put in console: document.querySelector("body").childNodes[5].innerHTML = "this is a test";
